b1ff3cdeaaeb92110c5de7fdb380c9f4b2bd39af
Konstantin Rolf <konstantin.rolf@gmail.com>
2021-03-10 00:14:44 +0100

    Added basic server manager package

--- servicemngr/servicemngr.py
+++ servicemngr/servicemngr.py
@@ -24,82 +24,29 @@ import json
 import logging
 import argparse
 
+
 from subprocess import Popen, PIPE
 from time import sleep
 
-from util_logger import addLogArgs, createLoggerFromArgs
-from util_arg import argReplaceCheck
-from util_cleanlog import cleanLogs
+from logutils.logger import addLogArgs, createLoggerFromArgs
+from logutils.arg import argReplaceCheck
     
-class ServiceManager:
-    """ The manager class responsible for checking and managing a server process. """
+def hello():
+    parent_parser = argparse.ArgumentParser(add_help=False)                                                                                                  
+    parent_parser.add_argument('--user', '-u', default=getpass.getuser(), help='username')                                                                                                                     
+    parent_parser.add_argument('--debug', default=False, required=False, action='store_true', dest="debug", help='debug flag')
+    
+    main_parser = argparse.ArgumentParser()                                                                                                                  
+    service_subparsers = main_parser.add_subparsers(title="service", dest="service_command")                                                                                                              
+    service_parser = service_subparsers.add_parser("first", help="first", parents=[parent_parser])                                                                                                             
+    action_subparser = service_parser.add_subparsers(title="action", dest="action_command")                                                                                                               
+    action_parser = action_subparser.add_parser("second", help="second", parents=[parent_parser])                                                                                                             
 
-    def __init__(self, data: dict, logger, timing:int=1):
-        """ Creates a service manager using the given config dictionary.
-
-        Parameters
-        ----------
-        data : dict
-            The config that is used to start up the services. See the given
-            config.json file for more information about the possible arguments.
-        logger :
-            The logger used to log 
-        """
-        self.data = data
-        self.timing = timing
-        self.logger = logger
-        try: # JSON Syntax checking
-            # (arg, type, required, <default>)
-            checks = [
-                ('clean_logs', bool, False, False),
-                ('services', list, False, [])
-            ]
-            serviceChecks = [
-                ('name', str, True), ('args', list, False, []),
-                ('exec', str, True), ('dir', str, False, './')
-            ]
-
-            # checks if the base dictionary satisfy the conditions
-            baseCheck = argReplaceCheck(self.data, checks)
-            if baseCheck is not None: # a check failed
-                raise AttributeError('Could not satisfy check: {}'.format(str(baseCheck)))
-
-            # checks if all services satisfy the conditions
-            for service in self.data['services']:
-                serviceCheck = argReplaceCheck(service, serviceChecks)
-                if serviceCheck is not None: # check failed
-                    raise AttributeError('Could not satisfy check: {}'.format(str(serviceCheck)))
-                # checks if the arg list satisfies the conditions
-                for arg in service['args']:
-                    if not isinstance(arg, str):
-                        raise AttributeError('Could not satisfy check: Arg elements must be str')
-        except Exception as e:
-            self.logger.error('Could not parse config file!\n{}'.format(e))
-            raise
-
-        # ==== All checks were done successfully ==== #
-
-        try:
-            # Cleans the logs if the config option is set
-            if self.data['clean_logs']:
-                self.logger.info('Cleaning log files')
-                cleanLogs()
-        except Exception as e:
-            self.logger.error('Could not clean logs!\n{}'.format(e))
-            raise
-        
-        self.serviceCount = len(self.data['services'])
-        self.services = [None] * self.serviceCount
-        self.startExceptions = [False] * self.serviceCount
-        self.args = [None] * self.serviceCount
-        # creates the commands used to start the services
-        for idx, service in enumerate(self.data['services']):
-            self.args[idx] = service['exec'].split(' ')
-            for arg in service['args']:
-                self.args[idx].extend(arg.split(' '))
-        
-
-    def startService(self, idx:int):
+class Service:
+    def __init__(self, args)
+        self.startException = False
+        self.args
+    def startService(self):
         """ Starts the given service using the given config.
 
         Parameters
@@ -120,6 +67,115 @@ class ServiceManager:
                 self.logger.error('Could not start {} with command \'{}\': Error {}'
                     .format(serviceInfo['name'], argsString, e))
                 self.startExceptions[idx] = True
+
+class ServiceManager:
+    """ A service manager class responsible for checking and managing a server process. """
+
+    SOURCE_PATH = 0
+    SOURCE_TEXT = 1
+    SOURCE_DICT = 2
+
+    @staticmethod
+    def addArgParserArguments(parser: argparse.ArgumentParser, useLogger:bool=True):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--file', type=str, default='config.json', 
+            help='Location of the config file to load (default: \'config.json\').')
+        parser.add_argument('--string', type=str, default=None,
+            help='Loads the config file as a string. This value cannot be used in combination'
+            'with the config file argument.')
+        parser.add_argument('--timing', type=int, default=5,
+            help='Interval between service checks (default: 5 seconds).')
+
+        if useLogger:
+            addLogArgs(parser, 'startup.log', 'ServiceSystem')
+
+    def __init__(self, source, tp:int, logger, timing:int=1):
+        """ Creates a service manager using the given config dictionary.
+        See the given config.json file for more information about the possible arguments.
+
+        Parameters
+        ----------
+        source
+            The source that is used to load the config which is then used to
+            start up the services. The value may be a [path], [text] or [dict]
+            depending on the value of the tp argument
+        tp : int
+            The type of the source. This value must be SOURCE_PATH, SOURCE_TEXT
+            or SOURCE_DICT.
+        logger :
+            The logger used to log 
+        """
+        self.timing = timing
+        self.logger = logger
+        if tp == SOURCE_PATH:
+            self.loadConfigFromPath(source)
+        elif tp == SOURCE_TEXT:
+            self.loadConfigFromString(source)
+
+    def loadConfigFromPath(self, data:str):
+        """ Loads a config file from a path. """
+        try:
+            data = None
+            with open(args.config, 'r') as cfg:
+                data = cfg.read()
+        except Exception as e:
+            self.logger.error('Could not load config file {}'.format(e))
+            raise
+        self.loadConfigFromString(data)
+
+    def loadConfigFromString(self, data:str):
+        """ Loads a config file from a source string. """
+        try:
+            dictData = json.load(data)
+        except Exception as e:
+            self.logger.error('Could not parse config string {}'.format(e))
+            raise
+        self.loadConfigFromDict(dictData)
+
+    def loadConfigFromDict(self, data:dict):
+        """ Loads a config file from a dictionary. """
+        try: # JSON Syntax checking
+            # (arg, type, required, <default>)
+            checks = [
+                ('clean_logs', bool, False, False),
+                ('services', list, False, [])
+            ]
+            serviceChecks = [
+                ('name', str, True), ('args', list, False, []),
+                ('exec', str, True), ('dir', str, False, './')
+            ]
+
+            # checks if the base dictionary satisfy the conditions
+            baseCheck = argReplaceCheck(data, checks)
+            if baseCheck is not None: # a check failed
+                raise AttributeError('Could not satisfy check: {}'.format(str(baseCheck)))
+
+            # checks if all services satisfy the conditions
+            for service in data['services']:
+                serviceCheck = argReplaceCheck(service, serviceChecks)
+                if serviceCheck is not None: # check failed
+                    raise AttributeError('Could not satisfy check: {}'.format(str(serviceCheck)))
+                # checks if the arg list satisfies the conditions
+                for arg in service['args']:
+                    if not isinstance(arg, str):
+                        raise AttributeError('Could not satisfy check: Arg elements must be str')
+        except Exception as e:
+            self.logger.error('Could not parse config file!\n{}'.format(e))
+            raise
+
+        # ==== All checks were done successfully ==== #
+        self.serviceCount = len(data['services'])
+        self.services = [None] * self.serviceCount
+        self.startExceptions = [False] * self.serviceCount
+        self.args = [None] * self.serviceCount
+        # creates the commands used to start the services
+        for idx, service in enumerate(data['services']):
+            self.args[idx] = service['exec'].split(' ')
+            for arg in service['args']:
+                self.args[idx].extend(arg.split(' '))
+        
+
+
 
     def checkService(self):
         """ Checks all services if they are up and running.
@@ -147,39 +203,26 @@ class ServiceManager:
             self.logger.info('Service {}:\t{}'.format(
                 index, 'UP' if isUp else 'DOWN'))
 
-    def repeat(self):
+    def repeat(self, exitCondition):
         """ Runs the checkService function repeatedly with timing difference
         
         Parameters
         ----------
         timing : float
             The amount of seconds to wait between calls of checkService
+        exitCondition : 
         """ 
         while True:
             self.checkService()
             sleep(float(self.timing))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Server Startup System (SSS)')
-    parser.add_argument('--config', type=str, default='config.json', 
-        help='Location of the config file to load (default: \'config.json\').')
-    parser.add_argument('--timing', type=int, default=5,
-        help='Interval between checks (default: 5 seconds).')
-    addLogArgs(parser, 'startup.log', 'ServerSystem')
 
+if __name__ == "__main__":
     args = parser.parse_args()
     # creates a logger from the supplied arguments
-    logger = createLoggerFromArgs(args)
+    
 
     logger.info('Starting up ServerSystem...')
-
-    try:
-        data = None
-        with open(args.config, 'r') as cfg:
-            data = json.load(cfg)
-    except Exception as e:
-        logger.error('Could not load config file {}'.format(e))
-        sys.exit(-1)
     
 
     mngr = ServiceManager(data, logger, timing=args.timing)
